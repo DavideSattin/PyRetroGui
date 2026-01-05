588719ff7b55a69cab02c81177e8fd631789c51e
Davide Sattin <davide.sattin@gmail.com>
2026-01-05 10:43:23 +0100

    Fix.Change Font

--- pyretrogui/ui_panel.py
+++ pyretrogui/ui_panel.py
@@ -26,4 +26,17 @@ class TextWidget(UIPanel):
           super().__init__()
 
       def update(self,context):
-          context.matrix[0][0] = CHAR_CLASSES["corner_tl"]
+          context.matrix[0][0]  = CHAR_CLASSES["corner_tl"]
+          context.matrix[0][-1] = CHAR_CLASSES["corner_tr"]
+          context.matrix[-1][0] = CHAR_CLASSES["corner_bl"]
+          context.matrix[-1][-1] = CHAR_CLASSES["corner_br"]
+
+          #Draw vertical lines.
+          for y in range(1, context.rows-1):
+              context.matrix[y][0] = CHAR_CLASSES["line_v"]
+              context.matrix[y][-1] = CHAR_CLASSES["line_v"]
+
+          #Draw orizzontal lines.
+          for x in range(1, context.cols -1):
+              context.matrix[0][x] = CHAR_CLASSES["line_h"]
+              context.matrix[-1][x] = CHAR_CLASSES["line_h"]
