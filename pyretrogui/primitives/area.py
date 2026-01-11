88744e25a4d5c117ca7c138edb733f1b478af271
DavideSattin <davide.sattin@gmail.com>
2026-01-11 10:59:59 +0100

    Work In Progress.

---
+++ pyretrogui/primitives/area.py
@@ -0,0 +1,24 @@
+# ==========================================
+# Project: PyRetroGUI
+# File: area
+# Author: Davide Sattin 
+# Created: 11/01/2026 10:38
+# Description: This class represents a rectangle area of the screen.
+# ==========================================
+from pyretrogui.primitives.location import Location
+from pyretrogui.primitives.size import Size
+from pyretrogui.primitives.view_port import ViewPort
+
+
+class Area:
+    def __init__(self, view_port: ViewPort):
+        self.top_left: Location = view_port.location
+        self.top_right: Location = self.top_left.add_x(view_port.size.width - 1)
+        self.bottom_left = self.top_left.add_y(view_port.size.height - 1)
+        self.bottom_right = self.bottom_left.add_x(view_port.size.width - 1)
+        self.center = self.top_left.add_x(view_port.size.width // 2).add_y(view_port.size.height // 2)
+        self.size: Size = view_port.size
+
+    @staticmethod
+    def create_area(location: Location, size: Size) -> "Area":
+        return Area(ViewPort(location, size))
--- pyretrogui/primitives/size.py
+++ pyretrogui/primitives/size.py
@@ -9,3 +9,6 @@ class Size:
       def __init__(self, width:int, height:int):
           self.width:int = width
           self.height:int = height
+
+      def __str__(self):
+          return f"Size(width={self.width}, height={self.height})"
