7cf9c8fb6d8eed6da7f0c67190da05199cde8afb
DavideSattin <davide.sattin@gmail.com>
2026-01-13 13:39:29 +0100

    WorkInProgress

---
+++ pyretrogui/ui_containers/dockable_panel.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from pygame.event import Event
+
+from pyretrogui.context import Context
+from pyretrogui.ui_elements.ui_element import UIElement
+from enum import Enum
+class DockMode(Enum):
+    NONE = 0
+    TOP = 1
+    BOTTOM = 2
+    LEFT = 3
+    RIGHT = 4
+    CONTENT = 5
+
+class DockableContainer(UIElement):
+
+      def __init__(self, parent):
+        super().__init__(parent)
+        self.containers: List[DockablePanel] = []
+
+      def update(self, context: Context):
+          pass
+
+      def on_key_event(self, event: Event, context: Context):
+          pass
+
+
+class DockablePanel(UIElement):
+      def __init__(self, parent):
+          super().__init__(parent)
+          self.dock_mode: DockMode = DockMode.NONE
