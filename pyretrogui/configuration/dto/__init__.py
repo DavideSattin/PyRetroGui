8ad93d7c50679fe30647b04870d5b0878d120cd9
DavideSattin <davide.sattin@gmail.com>
2026-01-25 18:43:27 +0100

    Work In Progress

---
+++ pyretrogui/configuration/dto/__init__.py
@@ -0,0 +1,7 @@
+# ==========================================
+# Project: PyRetroGUI
+# File: __init__.py
+# Author: Davide Sattin 
+# Created: 25/01/2026 18:28
+# Description:
+# ==========================================
