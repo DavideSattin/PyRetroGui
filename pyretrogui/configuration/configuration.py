f5e78a2035ece4f0fe5c951d9c807d75d4e4d293
DavideSattin <davide.sattin@gmail.com>
2026-01-30 16:27:49 +0100

    Video Buffer

--- pyretrogui/configuration/configuration.py
+++ pyretrogui/configuration/configuration.py
@@ -8,26 +8,27 @@
 from pyretrogui.configuration.dto.application_config import ApplicationConfig
 from pyretrogui.configuration.dto.font_config import FontConfig
 from pyretrogui.configuration.yaml_manager import YamlManager
+from pyretrogui.singleton_meta import SingletonMeta
 
 
-class Configuration:
+class Configuration(metaclass=SingletonMeta):
       def __int__(self):
            pass
 
       def load (self) -> ApplicationConfig:
           yaml_manager = YamlManager()
-          yaml_data = yaml_manager.load()
+          yaml_manager.load()
 
           # Root configuration.
           configuration_data = ApplicationConfig
-          configuration_data.title = yaml_data.get_data("application", "title", default="")
-          configuration_data.size = yaml_data.get_data("application", "size", default=(0,0))
+          configuration_data.title = yaml_manager.get_data("application", "title", default="")
+          configuration_data.size = yaml_manager.get_data("application", "size", default=(0,0))
 
           # Read the font configuration.
-          font_size = yaml_data.get_data("application", "font", "font_size", default=(8,16))
-          font_path = yaml_data.get_data("application", "font", "font_path", default="")
+          font_size = yaml_manager.get_data("application", "font", "font_size", default=(8,16))
+          font_path = yaml_manager.get_data("application", "font", "font_path", default="")
           configuration_data.font = FontConfig(font_size, font_path)
-
+          configuration_data.theme = yaml_manager.get_data("application", "theme", default="")
 
            # configuration_data.font_size = yaml_data.get_data("application", "font", "font_size", default="")
 
