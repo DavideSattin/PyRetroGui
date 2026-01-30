0c3f62b115c27662176f601b17a11ba8505d71b6
DavideSattin <davide.sattin@gmail.com>
2026-01-30 16:33:51 +0100

    Video Buffer

--- pyretrogui/configuration/configuration.py
+++ pyretrogui/configuration/configuration.py
@@ -7,6 +7,7 @@
 # ==========================================
 from pyretrogui.configuration.dto.application_config import ApplicationConfig
 from pyretrogui.configuration.dto.font_config import FontConfig
+from pyretrogui.configuration.dto.mouse_config import MouseConfig
 from pyretrogui.configuration.yaml_manager import YamlManager
 from pyretrogui.singleton_meta import SingletonMeta
 
@@ -28,6 +29,13 @@ class Configuration(metaclass=SingletonMeta):
           font_size = yaml_manager.get_data("application", "font", "font_size", default=(8,16))
           font_path = yaml_manager.get_data("application", "font", "font_path", default="")
           configuration_data.font = FontConfig(font_size, font_path)
+
+          # Read the mouse configuration.
+          enabled = yaml_manager.get_data("application", "mouse","enabled", True)
+          pointer= yaml_manager.get_data("application", "mouse", "pointer", True)
+          configuration_data.mouse = MouseConfig(enabled, pointer)
+
+          # Read the theme configuration.
           configuration_data.theme = yaml_manager.get_data("application", "theme", default="")
 
            # configuration_data.font_size = yaml_data.get_data("application", "font", "font_size", default="")
