5809ffe265851371e8259315b6504b5a2d450f43
Davide Sattin <davide.sattin@gmail.com>
2026-01-31 17:45:59 +0100

    Work In Progress

--- pyretrogui/graphic_context.py
+++ pyretrogui/graphic_context.py
@@ -12,10 +12,8 @@ from pyretrogui.apparence.theme import Theme
 from pyretrogui.configuration.dto.application_config import ApplicationConfig
 
 class GraphicContext:
-      def __init__(self, config: ApplicationConfig, theme: Theme):
+      def __init__(self):
           pygame.init()
-          self.config = config
-          self.theme = theme
           self.screen = None
           self.clock = pygame.time.Clock()
 
@@ -47,8 +45,12 @@ class GraphicContext:
 
       def draw_char(self, char:str, x: int, y: int, foreground_color: tuple[int,int ,int] =(255, 255, 255), background_color: tuple[int,int ,int] = (0,0,0)):
 
-          print("FG:", foreground_color, type(foreground_color))
-          print("BG:", background_color, type(background_color))
+          if background_color is None:
+              raise ValueError("background_color cannot be None")
+
+          if foreground_color is None:
+              raise ValueError("foreground_color cannot be None")
+
 
           surface = self.main_font.render(char, True, foreground_color, background_color)
           self.screen.blit(surface, (x, y))
