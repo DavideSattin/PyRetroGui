986195ed3bccdcbc21cc6a2e84194bc0d7e90654
DavideSattin <davide.sattin@gmail.com>
2026-02-08 22:15:05 +0100

    Containers WP

---
+++ pyretrogui/ui_elements/element_factory.py
@@ -0,0 +1,15 @@
+# ==========================================
+# Project: PyRetroGUI
+# File: element_factory
+# Author: Davide Sattin 
+# Created: 08/02/2026 13:55
+# Description:
+# ==========================================
+from typing import List
+
+from pyretrogui.ui_elements.ui_element import UIElement
+
+
+class ElementFactory:
+     def __int__(self):
+          self.elements: List[UIElement]  = []
