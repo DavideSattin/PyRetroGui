441679972c9f424061f2da7bee0318265892082e
Davide Sattin <davide.sattin@safety21.it>
2026-02-18 13:46:52 +0100

    Create Dockable Example 02

--- examples/Dockable01/main_container_01.py
+++ examples/Dockable01/main_container_01.py
@@ -7,14 +7,13 @@
 # ==========================================
 from pyretrogui.arranger.position_behaviour import PositionBehaviour
 from pyretrogui.arranger.resize_behaviour import ResizeBehaviour
-from pyretrogui.primitives.size import Size
 from pyretrogui.video.context import Context
 from pyretrogui.ui_containers.dockable_container import DockableContainer
 from pyretrogui.ui_containers.dockable_panel import DockablePanel
 from pyretrogui.ui_elements.ui_element import UIElement
 
 
-class MainContainer(DockableContainer):
+class MainContainer01(DockableContainer):
 
       def __init__(self, parent: UIElement):
           super().__init__(parent)
@@ -27,8 +26,8 @@ class MainContainer(DockableContainer):
           # Create the head container. Useful for menù or other stuff.
           header_container = DockablePanel(self)
           header_container.id = 12
-          header_container.margin = False
-          header_container.border = False
+          # header_container.margin = False
+          # header_container.border = False
           header_container.behaviour.size_behaviour = ResizeBehaviour.BUBBLE
           header_container.behaviour.position_behaviour = PositionBehaviour.DOCKED_TOP
           header_container.size.height = 1
@@ -45,8 +44,8 @@ class MainContainer(DockableContainer):
           # Create the footer container. Useful for status controls.
           footer_container = DockablePanel(self)
           footer_container.id= 13
-          footer_container.margin = False
-          footer_container.border = False
+          # footer_container.margin = False
+          # footer_container.border = False
           footer_container.behaviour.size_behaviour = ResizeBehaviour.BUBBLE
           footer_container.behaviour.position_behaviour = PositionBehaviour.DOCKED_BOTTOM
           footer_container.size.height = 1
