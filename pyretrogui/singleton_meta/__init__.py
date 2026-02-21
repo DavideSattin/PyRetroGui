b556cc8e96709d2cce9fc3e6b0550b464c3867cd
Davide Sattin <davide.sattin@gmail.com>
2026-02-21 10:44:40 +0100

    Singleton Metaclass for App and WidgetManager

---
+++ pyretrogui/singleton_meta/__init__.py
@@ -0,0 +1,7 @@
+# ==========================================
+# Project: PyRetroGUI
+# File: __init__.py
+# Author: Davide Sattin 
+# Created: 21/02/2026 10:44
+# Description:
+# ==========================================
