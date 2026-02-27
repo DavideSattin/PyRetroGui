51c6da325fdb639697d90552cc74f34f349865e0
DavideSattin <davide.sattin@gmail.com>
2026-02-27 13:29:51 +0100

    Fix All Examples!

--- examples/Dockable02/main_container_02.py
+++ examples/Dockable02/main_container_02.py
@@ -21,37 +21,24 @@ class MainContainer02(DockableContainer):
           self.border = False
 
       def init(self,context: Context):
-          super().init(context)
+
 
           # Create the head container. Useful for menù or other stuff.
-          header_container = DockablePanel(self)
-          header_container.id = 12
-          header_container.behaviour.size_behaviour = ResizeBehaviour.BUBBLE
-          header_container.behaviour.position_behaviour = PositionBehaviour.DOCKED_TOP
-          header_container.size.height = 1
+          header_container = super()._create_dockable_top_panel(1, "Top Container", (0, 0, 255))
           super().add_child(header_container)
 
           # Create the Contents container 01. Useful for editor or other stuff.
-          contents_container01 = DockablePanel(self)
-          contents_container01.behaviour.size_behaviour = ResizeBehaviour.BUBBLE
-          contents_container01.behaviour.position_behaviour = PositionBehaviour.CONTENT
-          contents_container01.background = (0, 255, 0)
+          contents_container01 = super()._create_dockable_content_panel("Container 1", (0, 255, 0))
           super().add_child(contents_container01)
 
-
           # Create the Contents container 02. Useful for editor or other stuff.
-          contents_container02 = DockablePanel(self)
-          contents_container02.behaviour.size_behaviour = ResizeBehaviour.BUBBLE
-          contents_container02.behaviour.position_behaviour = PositionBehaviour.CONTENT
-          contents_container02.background = (255, 0, 100)
+          contents_container02 = super()._create_dockable_content_panel("Container 1", (0, 255, 255))
           super().add_child(contents_container02)
 
           # Create the footer container. Useful for status controls.
-          footer_container = DockablePanel(self)
-          footer_container.id= 13
-          footer_container.behaviour.size_behaviour = ResizeBehaviour.BUBBLE
-          footer_container.behaviour.position_behaviour = PositionBehaviour.DOCKED_BOTTOM
-          footer_container.size.height = 1
-          footer_container.background = (255,0,0)
+          footer_container = super()._create_dockable_bottom_panel(1, "Footer Container", (255, 0, 0))
           super().add_child(footer_container)
+
+          # Se sposto prima va in errore.
+          super().init(context)
 
